0d4ce44682270937a486a10ae853baa7863193b9
vs462 <60940617+vs462@users.noreply.github.com>
2021-05-16 14:32:52 +0100

    fix saving functions

--- saving_functions.py
+++ saving_functions.py
@@ -1,5 +1,7 @@
 import json
 import boto3
+from pandas import json_normalize
+
 
 def save_jsonl_localy(data, output_path, append=False):
     mode = 'a+' if append else 'w'
@@ -17,11 +19,20 @@ def upload_to_s3(data, loc_file_path, file_name, bucket, prefix):
     s3_file_name = f"{prefix}/{file_name}"
    
     s3.Bucket(bucket).upload_file(loc_file_name, s3_file_name)
-    
-# loc_file_path = '/Users/vasilisa/desktop/api_pulls/data'
-# file_name = 'test_sm_google.jsonl'
-# bucket = 'cm-data-ops'    
-# prefix = 'testing'
+
+def list_buckets():
+    s3 = boto3.client('s3')   
+    response = s3.list_buckets()    
+    all_buckets = [bucket["Name"] for bucket in response['Buckets']]
+    all_buckets.insert(0, 'cm-data-ops')
+    # all_in_bucket = [my_bucket_object for my_bucket_object in my_bucket.objects.all()]
+    return all_buckets 
+
+def convert_to_table(responses):    
+    normalized_df = json_normalize(responses)
+    return normalized_df
+  
+
 
 
 
