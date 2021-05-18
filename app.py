f265b4303df4e0960240e9264b65aa5089ce7423
vs462 <60940617+vs462@users.noreply.github.com>
2021-05-18 19:10:55 +0100

    fix view/save table with sessionstate so no reload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 import json 
 import SessionState
 
-session_state = SessionState.get(data = None, checkboxed=False)
+session_state = SessionState.get(data = None, data_df = None, view = None)
 
 st.set_page_config(layout="wide")
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True) # make buttons display horizontally 
@@ -14,14 +14,15 @@ st.title('Pull data from APIs')
 
 def initialise(): 
     session_state.data = get_data()  
-  
-    st.markdown('<p class="sep-line"> </p>', unsafe_allow_html=True)    
+
     if session_state.data is not None:
+        st.markdown('<p class="sep-line"> </p>', unsafe_allow_html=True)       
         view_results(session_state.data)
             
 def get_data():  
-    data = session_state.data
-    api_list = ('Select', 'Appfollow', 'Trustpilot') 
+    data = session_state.data 
+    
+    api_list = ('Select', 'Appfollow', 'Trustpilot', 'Intercom') 
     col1, col2 = st.beta_columns([1,3]) # col 1 would take 1/3 of the width     
     st.sidebar.markdown(' ## Choose an API', unsafe_allow_html=True)
     tool = st.sidebar.selectbox("", api_list)   
@@ -29,10 +30,10 @@ def get_data():
     if tool =='Appfollow':
         from api_pulls.pull_from_appfollow import appfollow  
         data = appfollow(data)
-        
-    if tool =='Trustpilot':
-        from api_pulls.pull_from_trustpilot import trustpilot 
-        data = trustpilot(data)
+    
+    if tool =='Intercom':
+        from api_pulls.pull_from_intercom import intercom 
+        data = intercom(data)
         
     return data
 
@@ -42,25 +43,30 @@ def view_results(data):
     st.markdown(f'### Fetched {len(data)} responses', unsafe_allow_html=True)
     col1, col2, col3 = st.beta_columns(3)
 
-    with st.beta_expander('View as table'):
-        if st.checkbox('View'):
-            data_df = pd.DataFrame(data)
-            
-    with st.beta_expander('Save to S3 as JSONL'):
-        
+    with st.beta_expander('View as table'): 
+        if st.button('View') or session_state.view:
+            session_state.view = True
+            if session_state.data_df is None:
+                session_state.data_df = pd.DataFrame(data)
+            st.write(session_state.data_df)
+    
+    with st.beta_expander('Save as csv'):  
+        if st.button('Save'):
+            if session_state.data_df is None:
+                session_state.data_df = pd.DataFrame(data)
+            download_filename = st.text_input('Enter file name','file_name.csv', help='file_name.csv')
+            download_link(session_state.data_df, download_filename, 'Save as CSV')
+
+    with st.beta_expander('Save to S3 as JSONL'):      
         s3_location(data)
+
 
     with st.beta_expander('Save as json'):  
         data_json = json.dumps(data)
         download_filename = st.text_input('Enter file name','file_name.json',help='file_name.json')
         download_link(data_json, download_filename, 'Save as JSON')
 
-    with st.beta_expander('Save as csv'):  
-        data_df = pd.DataFrame(data)
-        download_filename = st.text_input('Enter file name','file_name.csv',help='file_name.csv')
-        download_link(data_df, download_filename, 'Save as CSV')
-        
-        
+    
 def download_link(object_to_download, download_filename, download_link_text):
     if isinstance(object_to_download, pd.DataFrame):
         object_to_download = object_to_download.to_csv(index=False)
@@ -69,11 +75,12 @@ def download_link(object_to_download, download_filename, download_link_text):
 
 def s3_location(data):
     loc_file_path = '/Users/vasilisa/desktop/api_pulls/data'  
-    file_name = st.text_input('Enter file name','test_sm_google.jsonl',help='some_name.jsonl')
+    file_name = st.text_input('Enter file name','file_name.jsonl')
     bucket = st.selectbox('Choose bucket', sf.list_buckets())     
-    prefix = st.text_input('Enter start prefix','testing',help='folder/subfolder')    
+    prefix = st.text_input('Enter prefix','folder/subfolder', help="Folder doesn't have to exist")    
     if st.button('save'):
-        sf.upload_to_s3(data, loc_file_path, file_name, bucket, prefix)
-    
+        msg = sf.upload_to_s3(data, loc_file_path, file_name, bucket, prefix)
+        st.markdown(f'### {msg}')
+      
 
 initialise()
